f6e0c33d17ddc8a71214000facd0605c31103bdb
fangsikai <15920372641@163.com>
2024-08-30 18:35:34 +0800

    Site updated: 2024-08-30 18:35:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/08/blog/","2024/08/08/hello-world/"];function toRandomPost(){
+var posts=["2024/08/30/CentOS安装OpenSSL/","2024/08/08/blog/","2024/08/08/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
