709053dea811e036e20576ffcf92c73ae0dcfa1c
fangsikai <15920372641@163.com>
2024-09-10 15:42:24 +0800

    Site updated: 2024-09-10 15:42:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/08/hello-world/","2024/08/30/linux/centos_openSSL/","2024/08/30/yashandb/yashandb_install/","2024/08/30/linux/centos_yum/"];function toRandomPost(){
+var posts=["2024/08/08/hello-world/","2024/08/30/linux/centos_openSSL/","2024/08/30/linux/centos_yum/","2024/08/30/yashandb/yashandb_install/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
